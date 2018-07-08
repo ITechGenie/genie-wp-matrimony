167872fae7dffd197c40eb6a33d668df03331f9d
prakashm88 <prakashm88@yahoo.co.in>
2018-07-09 00:19:02 +0530

    trying plugin activate

--- tests/test-gwpm-usercreate.php
+++ tests/test-gwpm-usercreate.php
@@ -12,6 +12,12 @@
  */
 class GwpmUserCreate extends WP_UnitTestCase {
 
+	function test_plugin_activation() {    
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+		$result = activate_plugin( WP_CONTENT_DIR . '/plugins/genie-wp-matrimony/genie-wp-matrimony-plugin.php', '', TRUE, FALSE );
+		$this->assertNotWPError( $result );
+	}
+
 	/**
 	 * create bulk users
 	 */
@@ -20,6 +26,8 @@ class GwpmUserCreate extends WP_UnitTestCase {
 		echo "Create Users Test " ;
 
 		$testHelper = new GwpmTestHelper() ;
+
+		$testHelper->setupMatrimony() ;
 
 		$testHelper->createUsers('GWPM-MALE', '10', '1') ;
 		$testHelper->createUsers('PRAK-FEMALE', '10', '2') ;
